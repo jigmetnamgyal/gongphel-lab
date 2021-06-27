31678994a01c7fa4195c0f3b5b4a0e1dd01d2457
JigmeT12 <jigmetashi02@gmail.com>
2021-06-27 11:04:49 +0600

    REMOVED: attachments

--- app/mailers/request_check_mailer.rb
+++ app/mailers/request_check_mailer.rb
@@ -4,9 +4,7 @@ class RequestCheckMailer < ApplicationMailer
   def send_req_check_mail
     @request = params
     @request_in_json = @request[:req].attributes
-    @pdf = "#{Rails.root}/lib/attachments/Gemfile.pdf"
-    @pdf_reader = File.binread(@pdf)
 
-    mail(to: "jigmetashi02@gmail.com", subject: "[Gongphel Team] Request status check", attachments: @pdf_reader)
+    mail(to: "jigmetashi02@gmail.com", subject: "[Gongphel Team] Request status check")
   end
 end
